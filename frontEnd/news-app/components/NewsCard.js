62759e30e73f9e2a7f9cfcd468f09b2355618330
Rifat Hossain <rifathossain328663@gmail.com>
2026-01-21 23:55:36 +0600

    Implement NewsCard component to display news details with navigation functionality

--- frontEnd/news-app/components/NewsCard.js
+++ frontEnd/news-app/components/NewsCard.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-const NewsCard = () => {
-  return <section aria-label={`news: ${title}`}></section>;
+const NewsCard = ({
+  title,
+  description,
+  image,
+  category,
+  source,
+  date,
+  url,
+}) => {
+  const badge = {};
+  function handleNavigation(url) {
+    window.open(url, "_blank", "noopener", "noreferrer");
+  }
+  return (
+    <section
+      className="news-card"
+      aria-label={`News: ${title}`}
+      style={{ cursor: "pointer" }}
+      onClick={() => handleNavigation(url)}
+    >
+      <img src={image} alt="" role="presentation" />
+      <div className="content">
+        <span aria-label={`News category: ${category}`} className="badge">
+          {category}
+        </span>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <footer>
+          <cite>Source: {source}</cite>
+          <time datetime={date}>{date}</time>
+        </footer>
+      </div>
+    </section>
+  );
 };
 
 export default NewsCard;
